c22d92619ac92223998d76dcd1aba19a0f4c54a7
Arun Jangid <ArunJangid@Aruns-MacBook-Pro.local>
2017-04-25 12:08:09 +0530

    Issue solved for Text Padding

--- Example/Pods/AJIBDesignables/AJIBDesignables/Classes/AJTextField.swift
+++ Example/Pods/AJIBDesignables/AJIBDesignables/Classes/AJTextField.swift
@@ -9,68 +9,63 @@
 import UIKit
 
 @IBDesignable
-public class AJTextField: UITextField {
+class AJTextField: UITextField {
 
     @IBInspectable var padding : CGPoint = CGPoint.zero
 
     
-    required override public init(frame: CGRect) {
+    required override init(frame: CGRect) {
         super.init(frame: frame)
         
     }
     
-    required public init?(coder aDecoder: NSCoder) {
+    required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
     }
-    
-    override public func awakeFromNib() {
-        super.awakeFromNib()
-    }
-    
-    convenience init() {
-        self.init(frame:CGRect.zero)
-    }
-    override public func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
         if padding.equalTo(CGPoint.zero) == false{
-            return self.bounds.insetBy(dx: padding.x, dy: padding.y)
+            return getBoundsPadding()
         }else{
             return super.placeholderRect(forBounds: self.bounds)
         }
     }
     
-    override public func textRect(forBounds bounds: CGRect) -> CGRect {
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
         if padding.equalTo(CGPoint.zero) == false{
-            return self.bounds.insetBy(dx: padding.x, dy: padding.y)
+            return bounds.insetBy(dx: padding.x, dy: padding.y)
         }else{
             return super.textRect(forBounds: self.bounds)
         }
     }
     
-    override public func borderRect(forBounds bounds: CGRect) -> CGRect {
+    override func borderRect(forBounds bounds: CGRect) -> CGRect {
         if padding.equalTo(CGPoint.zero) == false{
-            return self.bounds.insetBy(dx: padding.x, dy: padding.y)
+            return getBoundsPadding()
         }else{
             return super.borderRect(forBounds: self.bounds)
         }
     }
     
-    override public func editingRect(forBounds bounds: CGRect) -> CGRect {
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
         if padding.equalTo(CGPoint.zero) == false{
-            return self.bounds.insetBy(dx: padding.x, dy: padding.y)
+            return getBoundsPadding()
         }else{
             return super.editingRect(forBounds: self.bounds)
         }
     }
     
-    override public func clearButtonRect(forBounds bounds: CGRect) -> CGRect {
+    override func clearButtonRect(forBounds bounds: CGRect) -> CGRect {
         if padding.equalTo(CGPoint.zero) == false{
-            return self.bounds.insetBy(dx: padding.x, dy: padding.y)
+            return getBoundsPadding()
         }else{
             return super.clearButtonRect(forBounds: self.bounds)
         }
     }
     
+    func  getBoundsPadding() -> CGRect{
+       return UIEdgeInsetsInsetRect(bounds, UIEdgeInsetsMake(0, padding.x, 0, padding.y))
+    }
     
     
 }
